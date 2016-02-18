f75052746ba3351a3534472e7942ea31acb365f5
Kolovaitis <kna.209@yandex.ru>
2016-02-18 17:09:57 +0300

    Some bugs fixed2

---
+++ app/src/main/java/kolovaitis/by/chat/AnimationView.java
@@ -0,0 +1,26 @@
+package kolovaitis.by.chat;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by user on 16.02.2016.
+ */
+public class AnimationView extends View {
+
+    public AnimationView(Context context) {
+        super(context);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+      //  canvas.drawColor(Color.GREEN);
+
+    }
+    public void fly(){
+
+    }
+    private void init() {}
+}
