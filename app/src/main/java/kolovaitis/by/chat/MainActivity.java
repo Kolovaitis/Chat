66703e487902dba6f4e34f93a81884add1323f2a
Kolovaitis <kna.209@yandex.ru>
2016-02-25 21:26:39 +0300

    fire

--- app/src/main/java/kolovaitis/by/chat/MainActivity.java
+++ app/src/main/java/kolovaitis/by/chat/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.StrictMode;
@@ -15,6 +16,7 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -35,19 +37,17 @@ public class MainActivity extends AppCompatActivity {
     public static String currentMessage = null;
     public static int[] colors = new int[6];
     public static int pastColor = 3;
+    public boolean wasFirst = true;
+    public long mynumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        colors = new int[]{Color.parseColor("#ff9800"),
-                Color.parseColor("#ff5722"),
-                Color.parseColor("#00bcd4"),
-                Color.parseColor("#ffeb3b"),
-                Color.parseColor("#607d8b"),
-                Color.parseColor("#795548")
-        };
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        frameAnimation.start();
         ((LinearLayout) findViewById(R.id.linearLayout)).addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
@@ -82,23 +82,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void send(View v) {
+int sum=0;
+        String s=((EditText) findViewById(R.id.editText)).getText()+"";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=0){
 
+                sum=1;
+                break;
+            }
+        }if(sum!=0){
         MainActivity.currentMessage = ((EditText) findViewById(R.id.editText)).getText() + "";
         Log.d("sending message", ((EditText) findViewById(R.id.editText)).getText() + "");
-        ((EditText) findViewById(R.id.editText)).setText("");
+        ((EditText) findViewById(R.id.editText)).setText("");}
     }
 
     public void work() {
         Preferences pref = Preferences.systemRoot();
         if (pref.getBoolean("flag", false)) {
-
-
+            String string = pref.get("data", "no data");
             pref.putBoolean("flag", false);
             Log.d("text", pref.get("data", "no data"));
             LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
             LinearLayout linear = new LinearLayout(this);
             linear.setOrientation(LinearLayout.VERTICAL);
-            String string = pref.get("data", "no data");
+
             TextView human = new TextView(this);
             TextView textView = new TextView(this);
             try {
@@ -109,23 +116,34 @@ public class MainActivity extends AppCompatActivity {
 
                 human.setPadding(0, 20, 0, 0);
 
+
                 linear.addView(human);
                 textView.setGravity(Gravity.LEFT);
                 textView.setText(string.substring(string.indexOf(":") + 1));
 
-               // linear.setGravity(View.TEXT_ALIGNMENT_CENTER);
+                // linear.setGravity(View.TEXT_ALIGNMENT_CENTER);
             } catch (Exception e) {
                 textView.setText(string);
                 textView.setGravity(Gravity.CENTER);
 
             }
-
+            if (wasFirst) {
+                wasFirst = false;
+                mynumber = Long.parseLong((textView.getText() + "").substring(6));
+            }
             textView.setTextSize(20);
-            textView.setBackgroundResource(R.drawable.message);
+            textView.setBackgroundResource(R.drawable.opponent);
+            textView.setPadding(100, 0, 70, 20);
+            if (human.getText().toString().contains(mynumber + "")) {
+                textView.setBackgroundResource(R.drawable.me);
+                human.setGravity(Gravity.RIGHT);
+                textView.setPadding(70, 0, 100, 20);
+            }
 
 
-            textView.setPadding(20, 0, 20, 5);
 
+            textView.setTextColor(Color.parseColor("#9e9e9e"));
+            human.setTextColor(Color.parseColor("#9e9e9e"));
             linear.addView(textView);
             layout.addView(linear);
 
